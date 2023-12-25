1c4cef2a3e8c63def40803b7d40e07c0dfae5c65
sho ochiai <ochi1020@gmail.com>
2023-12-25 22:07:45 +0900

    feat: add split windows keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
 local keymap = vim.keymap
 
 keymap.set("i", "jk", "<Esc>", { desc = "Switch to Normal mode." })
+
+-- split windows
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "split vartical right." })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "split horizontal below." })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "?" })
+keymap.set("n", "<leader>sx", ":close<CR>", { desc = "close window." })
