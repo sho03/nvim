31c3d50bc0ddf9978da4dd51e930e2e7c3876e0d
sho ochiai <ochi1020@gmail.com>
2024-01-29 21:32:00 +0900

    feat: add keymaps for split window.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,9 @@
 local keymap = vim.keymap
 
+-- to normal mode.
 keymap.set('i', 'jk', '<ESC>')
+
+-- split window
+keymap.set("n", "ss", ":split<Return><C-w>w")
+keymap.set("n", "sv", ":vsplit<Return><C-w>w")
+
