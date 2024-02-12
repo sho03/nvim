2e8f06a20002566f64ade6ae6bba2113d9e2a038
sho ochiai <ochi1020@gmail.com>
2024-02-12 21:42:06 +0900

    fix: typo that occur a error.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,7 +4,7 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylelua,
+        null_ls.builtins.formatting.stylua,
       }
     })
   end
