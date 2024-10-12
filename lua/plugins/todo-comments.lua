55a4823fda9cebd2306f0539318fc627393cc741
sho ochiai <ochi1020@gmail.com>
2024-10-12 10:05:55 +0900

    fix: error

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -6,9 +6,4 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
-  require("todo-comments").setup {
-    highlight = {
-      pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
-    }
-  }
 }
