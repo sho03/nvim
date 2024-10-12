35683ea78ef23adad5aef2b3cbb66b85fdb2dbd0
sho ochiai <ochi1020@gmail.com>
2024-10-12 18:21:10 +0900

    feat: add auto completion with cmp-nvim-lsp

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -2,10 +2,17 @@ return {
   "neovim/nvim-lspconfig",
   config = function()
     local lspconfig = require("lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    lspconfig.ts_ls.setup({})
-    lspconfig.html.setup({})
-    lspconfig.cssls.setup({})
+    lspconfig.ts_ls.setup({
+      capabilities = capabilities
+    })
+    lspconfig.html.setup({
+      capabilities = capabilities
+    })
+    lspconfig.cssls.setup({
+      capabilities = capabilities
+    })
   end
 }
 
